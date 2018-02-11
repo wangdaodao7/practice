a91676518e50e9e41f8a3e1bb34a7bb4af2228fb
wangdaodao7 <ahking0516@126.com>
2018-02-11 21:27:08 +0800

    update

--- 成绩分析.py
+++ 成绩分析.py
@@ -3,8 +3,8 @@
 """
     作者:     王导导
     版本:     1.0
-    日期:     2018/02/09
-    项目名称： 成绩分析
+    日期:     2018/02/11
+    项目名称： 成绩数据分析
     项目参考地址：http://blog.csdn.net/qq_14959801/article/details/51382207
 
 """
@@ -19,6 +19,7 @@ custom_font = mpl.font_manager.FontProperties(fname = '/Users/wangdaodao/Python/
 
 font_size = 10
 fig_size = (8, 6)
+bar_width = 0.35
 
 name = ('小刚', '小芳')
 subjects = ('物理', '化学', '生物')
@@ -26,6 +27,7 @@ scores = ((65, 80, 72), (70, 90, 85))
 
 mpl.rcParams['font.size'] = font_size
 mpl.rcParams['figure.figsize'] = fig_size
+
 
 index = np.arange(len(scores[0]))
 
