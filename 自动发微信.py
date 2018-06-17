dbb1ab0e9d3c629c7877df73589d3ccaa780494b
wangdaodao7 <ahking0516@126.com>
2018-06-17 21:30:04 +0800

    u

--- 自动发微信.py
+++ 自动发微信.py
@@ -8,9 +8,9 @@ import requests
 from bs4 import BeautifulSoup
 
 itchat.auto_login(hotReload=True)
-users = itchat.search_friends('杨欢')
-username = users[0]['UserName']
-print('登陆成功！！！')
+# users = itchat.search_friends('杨欢')
+# username = users[0]['UserName']
+# print('登陆成功！！！')
 
 
 def joke(num):
@@ -22,15 +22,23 @@ def joke(num):
     return txt2[0]
 
 
+# while True:
+#     page = random.choices(range(1, 13))
+#     msg = joke(page)
+#     if len(msg) < 20:
+#         page2 = random.choices(range(1, 13))
+#         msg = joke(page2)
+#         print(page2)
+#     print(msg, page)
+#     itchat.send('【笑话】{}'.format(msg), toUserName=username)
 
-while True:
-    msg = joke(random.choices(range(1, 20)))
-    if len(msg) < 20:
-        msg = joke(random.choices(range(1, 20)))
-    print(msg)
-    itchat.send('【半小时笑话】{}'.format(msg), toUserName=username)
+#     time.sleep(3)
 
-    time.sleep(10)
+
+
+
+
+
 
 msgs = ['小仙女，想你。现在是{min}:0{sec}，该第喝{num}杯水啦！',
         '最爱你的人温馨提示：现在是{min}:0{sec}，是时候喝{num}杯水啦！', '{min}:0{sec},喝第{num}杯水吧，我的小可爱！',
@@ -39,7 +47,6 @@ msgs = ['小仙女，想你。现在是{min}:0{sec}，该第喝{num}杯水啦！
 msg_heshui = ['喝了没？', '赶紧去喝', '听我的话', '不喝我一直发，', '毛主席说，爱喝水的仙女才是好仙女']
 msg_qilai = ['还在坐着吗', '别懒了，现在就起来吧',
              '小仙女，为了你的健康，还是啰嗦下，起来走走吧', '起来走俩步，小可爱', '起来走走，活到99']
-
 
 
 def go():
@@ -67,3 +74,11 @@ def go():
         #     itchat.send('【半小时笑话】{}'.format(msg), toUserName=username)
         time.sleep(1)
         print('{0}:{1}:{2}'.format(now_hour, now_minute, now_second))
+
+
+
+
+# rooms = itchat.get_chatrooms(update=True)
+rooms = itchat.search_chatrooms('表情包')
+
+print(rooms[0])
