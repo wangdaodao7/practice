1b84e9867fb57c131c730019477d2c9ccc883db9
wangdaodaodao <ahking0516@126.com>
2018-09-05 21:07:00 +0800

    uuu

--- collections.py
+++ collections.py
@@ -16,3 +16,12 @@ for infile in glob.glob('*.png'):
 # im = Image.open(r'D:\python\1111\h\AppIcon60x60@2x.png')
 # im2 = im.resize((400, 400), Image.ANTIALIAS)
 # im2.show()
+
+for root, dirs, files in os.walk(r'D:\video'):
+	# print(files)
+	for file in files:
+		
+		if 'mp4' in file:
+			print('--------------{}---------------------'.format(file))
+		else:
+			print(file ,type(file))
