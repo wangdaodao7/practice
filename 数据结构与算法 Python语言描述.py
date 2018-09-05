05ba3c88fb23056743f8aa326a328d07af6a2f9f
wangdaodao7 <ahking0516@126.com>
2018-09-05 20:13:45 +0800

    uuuu

--- 数据结构与算法 Python语言描述.py
+++ 数据结构与算法 Python语言描述.py
@@ -8,6 +8,24 @@
     项目参考地址：无
 
 """
+from collections import Counter
 
+
+
+aa = '''<meta property="og:description" content="步骤： 1. 下载Git客户端 2. 配置环境变量 3. 设置vscode与Git的关联 4. 重启 步骤一： 该网址，下载即可。 https://git scm.com/downloads 步骤二：" />
+<link type="text/css" rel="stylesheet" href="/bundles/blog-common.css?v=giTNza-Of-PEt5UsELhFQAR7G6-bfaSa4oolcq7i9-o1"/>
+<link id="MainCss" type="text/css" rel="stylesheet" href="/skins/SimpleMemory/bundle-SimpleMemory.css?v=EanP3quXYv9G0oFCmz4BNBHvmK7M_Si1iD1EpUHL_441"/>
+<link id="mobile-style" media="only screen and (max-width: 767px)" type="text/css" rel="stylesheet" href="/skins/SimpleMemory/bundle-SimpleMemory-mobile.css?v=XGOE8_i3XCsAIQHcgl5c_8VAMMKRkj0W3Eckyc8qbso1"/>
+<link title="RSS" type="application/rss+xml" rel="alternate" href="https://www.cnblogs.com/geekfeier/rss"/>
+<link title="RSD" type="application/rsd+xml" rel="EditURI" href="https://www.cnblogs.com/geekfeier/rsd.xml"/>
+<link type="application/wlwmanifest+xml" rel="wlwmanifest" href="https://www.cnblogs.com/geekfeier/wlwmanifest.xml"/>
+<script src="//common.cnblogs.com/scripts/jquery-2.2.0.min.js"></script>
+<script type="text/javascript">var currentBlogApp = 'geekfeier', cb_enable_mathjax=false;var isLogined=false;</script>
+<script src="/bundles/blog-common.js?v=yRkjgN2sBQkB4hX-wirHxPomeBT9sB5dawr6ob7KIvg1" type="text/javascript"></script>
+</head>
+<body>
+<a name="top"></a>
+'''
+print(Counter(aa))
 
 
